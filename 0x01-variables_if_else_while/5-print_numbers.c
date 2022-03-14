8d7674fa3d60c19eccbb86f374675b0332e6231f
KENNEDY-ONGESA <kenmejja@gmail.com>
2022-03-14 05:33:18 -0700

    Program to print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 38
+
+	while (i < 48)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
