cbf05435081112326017c4129bc08e805de71a6b
Bboxx Kenya IT <kenmejja@gmail.com>
2022-03-22 00:27:35 +0300

    Advanced functions

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nb;
+
+	nb = _atoi("");
+	printf("%d\n", nb;
+	return (0);
+}
+
+
