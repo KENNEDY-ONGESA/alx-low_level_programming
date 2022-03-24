efef4e924dc4b182bcaddf6d4aff8e894c23c44b
KENNEDY-ONGESA <kenmejja@gmail.com>
2022-03-24 12:11:06 +0300

    Function that returns string comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,30 @@
+#include "main.h"
 
+/**
+ * main - function that works exactly like strcmp
+ *
+ * Description - print a function that works as strcmp
+ *
+ * Return: Always 0 (success)
+ */
+
+int my_strcmp(char s1[], char s2[])
+{
+	for(int i = 0; ; i++)
+	{
+		if(s1[i] == '\0' && s2[i] == '\0')
+		{
+			return 0;
+		}
+		int a1 = (int) s1[i];
+		int a2 = (int) s2[i];
+		if(s1 > s2)
+		{
+			return 1;
+		}
+		if(s2 > s1)
+		{
+			return -1;
+		}
+	}
+}
