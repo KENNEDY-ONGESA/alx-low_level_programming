735f9cd63868e6f200880b971856dbb4b8a948d2
KENNEDY-ONGESA <kenmejja@gmail.com>
2022-04-21 17:44:12 +0300

    Assembly Language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,16 @@
-section .text
-            global _start
-_start:
-            mov edx, len
-            mov ecx, msg
-            mov ebx, 1
-            mov eax, 4
-	    int 0x80
+        global    _start
 
-           mov eax, 1
-           int 0x80
-section .data
-             msg db "Hello, Holberton", 0xa
-             len equ $ -msg
+        section .data
+hello:
+        db "Hello, World", 10
+
+        section   .text
+_start:
+        mov       rax, 1
+        mov       rdi, 1
+        mov       rsi, hello
+        mov       rdx, 13
+        syscall
+        mov       rax, 60
+        xor       rdi, rdi
+        syscall
